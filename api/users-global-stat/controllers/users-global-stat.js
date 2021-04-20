f70e7522a838db946508cabef1ae1cef98e99d15
Mathieu Daix <mathieudaixpro@gmail.com>
2021-04-20 20:02:02 +0200

    add leaderbord routes

--- api/users-global-stat/controllers/users-global-stat.js
+++ api/users-global-stat/controllers/users-global-stat.js
@@ -5,14 +5,38 @@
  * to customize this controller
  */
 
- const atob = require('atob')
+const { sanitizeEntity } = require('strapi-utils')
+const atob = require('atob')
 
- module.exports = {
-   async me (ctx) {
-     const { id } = JSON.parse(atob(ctx.headers.authorization.split('.')[1].replace('-', '+').replace('_', '/')))
-     
-     return await strapi.query('users-global-stat').find({
-       user_id: id,
-      })
-   }
- }
+module.exports = {
+  async me(ctx) {
+    const { id } = JSON.parse(atob(ctx.headers.authorization.split('.')[1].replace('-', '+').replace('_', '/')))
+
+    return await strapi.query('users-global-stat').find({
+      user_id: id,
+    })
+  },
+
+  async leaderboard() {
+    const maxSpeed = await strapi.query('users-global-stat').find({
+      _limit: 10,
+      _sort: 'vMax:DESC'
+    })
+
+    const maxDistance = await strapi.query('users-global-stat').find({
+      _limit: 10,
+      _sort: 'totalDistance:DESC'
+    })
+
+    const totalDuration = await strapi.query('users-global-stat').find({
+      _limit: 10,
+      _sort: 'totalTravelDuration:DESC'
+    })
+
+    const maxSpeedSanitized = sanitizeEntity(maxSpeed, { model: strapi.models['users-global-stat'] })
+    const maxDistanceSanitized = sanitizeEntity(maxDistance, { model: strapi.models['users-global-stat'] })
+    const totalDurationSanitized = sanitizeEntity(totalDuration, { model: strapi.models['users-global-stat'] })
+
+    return { maxSpeedSanitized, maxDistanceSanitized, totalDurationSanitized }
+  }
+}
