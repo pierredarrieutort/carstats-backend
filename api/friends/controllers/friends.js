d63e5978972956abaa50f053dcb767f7b09fb1b8
Pierre DARRIEUTORT <p.darrieutort@outlook.fr>
2021-05-30 17:24:37 +0200

    friends system is now properly working

--- api/friends/controllers/friends.js
+++ api/friends/controllers/friends.js
@@ -5,4 +5,51 @@
  * to customize this controller
  */
 
-module.exports = {};
+const atob = require('atob')
+
+module.exports = {
+  async me (ctx) {
+    const { id } = JSON.parse(atob(ctx.headers.authorization.split('.')[1].replace('-', '+').replace('_', '/')))
+    const { friendRequests, receivedFriends } = await strapi.query('user', 'users-permissions').findOne({ id })
+    const groupedFriends = [...friendRequests, ...receivedFriends]
+
+    const myFriends = await friendRequestParser(groupedFriends, 'accepted')
+    const pendingRequests = await friendRequestParser(receivedFriends, 'pending')
+    const blockUsers = await friendRequestParser(groupedFriends, 'blocked')
+    const sendedRequests = await friendRequestParser(friendRequests, 'pending')
+
+    ctx.send({
+      myFriends,
+      pendingRequests,
+      blockUsers,
+      sendedRequests
+    })
+  }
+}
+
+/**
+ * @description Parse and sanitize users friendships.
+ * @param {Array} list Provide a Strapi users array.
+ * @param {String=} statusFilter OPTIONAL - Pass wanted filtering status in Strapi friendships.
+ * @returns {Array} Parsed array of objects.
+ */
+async function friendRequestParser (list, statusFilter) {
+  return await Promise.all(list
+    .filter(({ status }) => statusFilter ? status === statusFilter : true)
+    .map(async ({ status, friend_requester, user_target }) => {
+      const { id: fromId, username: fromUsername } = await strapi.query('user', 'users-permissions').findOne({ id: friend_requester })
+      const { id: toId, username: toUsername } = await strapi.query('user', 'users-permissions').findOne({ id: user_target })
+
+      return {
+        status,
+        from: {
+          id: fromId,
+          username: fromUsername
+        },
+        to: {
+          id: toId,
+          username: toUsername
+        }
+      }
+    }))
+}
