b13c48a2e6308938f22e50e025ad6238c6516723
Pierre DARRIEUTORT <p.darrieutort@outlook.fr>
2021-06-02 12:58:15 +0200

    [WIP] - Progressing on #132

--- api/friends/controllers/friends.js
+++ api/friends/controllers/friends.js
@@ -24,7 +24,59 @@ module.exports = {
       pendingRequests,
       blockedUsers
     })
+  },
+
+  async createViaID (ctx) {
+    const { requesterID } = JSON.parse(atob(ctx.headers.authorization.split('.')[1].replace('-', '+').replace('_', '/')))
+    const userTarget = await strapi.query('user', 'users-permissions').findOne({ username: ctx.request.body.username })
+
+    if (!userTarget) {
+      unableToGetUser(ctx)
+    }
+
+    // Group all userTarget friendships
+    const groupedFriends = [...userTarget.friendRequests, ...userTarget.receivedFriends]
+
+
+    const friendshipChecker = groupedFriends
+      .find(({ friendRequester, userTarget }) => userTarget === requesterID || friendRequester === requesterID)
+
+    if (friendshipChecker) {
+      switch (friendshipChecker.status) {
+        case 'blocked':
+          // If the Requester blocked target user, he's able to pass friendship to pending (new friend request)
+          if (friendshipChecker.lastActionAuthor.id === requesterID) {
+            // TODO Update friendship status to PENDING
+          } else {
+            unableToGetUser(ctx)
+          }
+          break
+        case 'pending':
+          if (friendshipChecker.lastActionAuthor.id === requesterID) {
+            // TODO Create message : the request already sended
+          } else {
+            // TODO Update friendship status to ACCEPTED
+          }
+          break
+        case 'accepted':
+            // TODO ctx returns message : already friends
+          break
+      }
+
+    } else {
+      // TODO Create friendship relation : requester, target, status = pending
+    }
+
+
+
+    ctx.send(groupedFriends)
+    console.log('b', userTarget)
+    // function to check friendship status
   }
+}
+
+function unableToGetUser (ctx) {
+  return ctx.throw(404, { message: 'No user found' })
 }
 
 /**
@@ -36,9 +88,9 @@ module.exports = {
 async function friendRequestParser (list, statusFilter) {
   return await Promise.all(list
     .filter(({ status }) => statusFilter ? status === statusFilter : true)
-    .map(async ({ id: friendshipID, status, friend_requester, user_target }) => {
-      const { id: fromId, username: fromUsername } = await strapi.query('user', 'users-permissions').findOne({ id: friend_requester })
-      const { id: toId, username: toUsername } = await strapi.query('user', 'users-permissions').findOne({ id: user_target })
+    .map(async ({ id: friendshipID, status, friendRequester, userTarget }) => {
+      const { id: fromId, username: fromUsername } = await strapi.query('user', 'users-permissions').findOne({ id: friendRequester })
+      const { id: toId, username: toUsername } = await strapi.query('user', 'users-permissions').findOne({ id: userTarget })
 
       return {
         friendshipID,
